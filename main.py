5513f1d74763548a58e89459ae2a7ad3f71db6ff
Odecam1 <timotheedanymacedo@gmail.com>
2023-10-19 13:19:55 +0200

    arangement du code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 import random
 
-CHANNEL_ID = "oiuytr"
+CHANNEL_ID = ""
 
 intents = discord.Intents.default()
 intents.members = True
@@ -12,23 +12,27 @@ async def on_member_join(member):
     channel = bot.get_channel(CHANNEL_ID)
     await channel.send(f"Salut {member.mention} ! Bienvenue sur le serveur.")
 
+
 @bot.command()
 async def server_info(ctx):
     server = ctx.guild
     total_members = len(server.members)
-    online_members = sum(member.status == discord.Status.online for member in server.members)
+    online_members = sum(
+        member.status == discord.Status.online for member in server.members
+    )
     total_channels = len(server.channels)
-    
+
     response = f"Nombre total de membres : {total_members}\nMembres en ligne : {online_members}\nNombre de canaux : {total_channels}"
     await ctx.send(response)
+
 
 @bot.command()
 async def create_poll(ctx, question, *options):
     poll_embed = discord.Embed(title=question)
     for option in options:
         poll_embed.add_field(name=option, value="0", inline=False)
-    
+
     poll_message = await ctx.send(embed=poll_embed)
-    
+
     for i in range(len(options)):
-        await poll_message.add_reaction(f"{i+1}\u20e3")  # Utilisation des réactions numérotées
+        await poll_message.add_reaction(f"{i+1}\u20e3")
